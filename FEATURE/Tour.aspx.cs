6272b88c33a67e7a34fd86d0deae90847e6dd8a0
Minh Tử <70022868+minh7tu@users.noreply.github.com>
2021-10-03 20:55:40 +0700

    Cập nhập giao diện tour

--- FEATURE/Tour.aspx.cs
+++ FEATURE/Tour.aspx.cs
@@ -9,6 +9,11 @@ public partial class FEATURE_Tour : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+       
+    }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
 
     }
 }
