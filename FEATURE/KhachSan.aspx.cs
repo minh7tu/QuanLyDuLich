35f114a24a326c0745cf549d2e10d7f6e9401c25
LEMANH444 <79786250+LEMANH444@users.noreply.github.com>
2021-10-03 21:13:38 +0700

    cập nhật sự kiện

--- FEATURE/KhachSan.aspx.cs
+++ FEATURE/KhachSan.aspx.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
+using System.Web.Configuration;
 public partial class FEATURE_KhachSan : System.Web.UI.Page
 {
+    
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
     }
 }
